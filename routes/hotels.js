b67054be2758eeac1068a5d7cb0ae845466e730f
mitibirru <357miteshbirru@gmail.com>
2022-07-03 16:38:10 +0530

    added essential apis

---
+++ routes/hotels.js
@@ -0,0 +1,57 @@
+import express from "express";
+import Hotel from "../models/Hotel.js";
+import { createError } from "../utils/error.js";
+import { createHotel } from "./controllers/hotel.js";
+
+const router = express.Router();
+
+// CREATE
+router.post("/", createHotel);
+
+// UPDATE
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedHotel = await Hotel.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedHotel);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// DELETE
+router.delete("/:id", async (req, res) => {
+  try {
+    await Hotel.findByIdAndDelete(req.params.id);
+    res.status(200).json("Hotel deleted");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// GET
+router.get("/:id", async (req, res) => {
+  try {
+    const hotel = await Hotel.findById(req.params.id);
+    res.status(200).json(hotel);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// GET ALL
+router.get("/", async (req, res, next) => {
+  try {
+    const hotels = await Hotel.find();
+    res.status(200).json(hotels);
+  } catch (err) {
+    next(err);
+  }
+});
+
+export default router;
